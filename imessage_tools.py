6b6d81f74c81111e1a521b64695726463080222a
mateo <mateo.jacome@hotmail.com>
2024-02-20 21:12:29 -0500

    hello good people

---
+++ imessage_tools.py
@@ -0,0 +1,92 @@
+import sqlite3
+import datetime
+import pytz
+
+second = 1000000000
+
+def get_chat_mapping(db_location):
+    conn = sqlite3.connect(db_location)
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT room_name, display_name FROM chat")
+    result_set = cursor.fetchall()
+
+    mapping = {room_name: display_name for room_name, display_name in result_set}
+
+    conn.close()
+
+    return mapping
+
+def apple_date_to_datetime(date):
+    jan2001 = datetime.datetime.fromisoformat('2001-01-01')
+    jan2001 = jan2001.replace(tzinfo=pytz.UTC)
+
+    new_date = jan2001 + datetime.timedelta(seconds=date/second)
+
+    eastern_timezone = pytz.timezone('US/Eastern')
+    new_date = new_date.astimezone(eastern_timezone)
+
+    return new_date
+
+def read_messages(db_location, n=10, self_number='Me', human_readable_date=True, handle_identifyer=1):
+    conn = sqlite3.connect(db_location)
+    cursor = conn.cursor()
+
+    query = """
+    SELECT message.ROWID, message.date, message.text, message.attributedBody, message.handle_id, message.is_from_me, message.cache_roomnames
+    FROM message
+    LEFT JOIN handle ON message.handle_id = handle.ROWID
+    """
+
+    query += f"WHERE message.handle_id = {handle_identifyer}"
+
+    if n is not None:
+        query += f" ORDER BY message.date DESC LIMIT {n}"
+
+
+    results = cursor.execute(query).fetchall()
+    messages = []
+
+    for result in results:
+        rowid, date, text, attributed_body, handle_id, is_from_me, cache_roomname = result
+
+        if handle_id is None:
+            phone_number = self_number
+        else:
+            phone_number = handle_id
+
+        if text is not None:
+            body = text
+        elif attributed_body is None:
+            continue
+        else:
+            attributed_body = attributed_body.decode('utf-8', errors='replace')
+
+            if "NSNumber" in str(attributed_body):
+                attributed_body = str(attributed_body).split("NSNumber")[0]
+                if "NSString" in attributed_body:
+                    attributed_body = str(attributed_body).split("NSString")[1]
+                    if "NSDictionary" in attributed_body:
+                        attributed_body = str(attributed_body).split("NSDictionary")[0]
+                        attributed_body = attributed_body[6:-12]
+                        body = attributed_body
+
+        if "Loved “" in body or "Emphasized “" in body or "Laughed at “" in body or "Liked “" in body or "Disliked “" in body or "Questioned “" in body or "20 Questions" == body or "Mancala" == body:
+            continue
+
+        if human_readable_date:
+            date = apple_date_to_datetime(date)
+
+        mapping = get_chat_mapping(db_location)
+
+        try:
+            mapped_name = mapping[cache_roomname]
+        except:
+            mapped_name = None
+
+        messages.append(
+            {"rowid": rowid, "date": date, "body": body, "phone_number": phone_number, "is_from_me": is_from_me,
+            "cache_roomname": cache_roomname, 'group_chat_name' : mapped_name})
+
+    conn.close()
+    return messages
---
+++ main.py
@@ -0,0 +1,138 @@
+from imessage_tools import *
+from nltk.stem.snowball import SnowballStemmer
+import matplotlib.pyplot as plt
+import re
+
+# Path to the chat.db file
+chat_db = "chat.db"
+
+# Phone number or label for "you"
+self_number = "Me"
+
+# Number of messages to return
+n = None
+
+# Path to the stoplist file
+stop_list = "SmartStoplist.txt"
+
+def print_date(date):
+    return date.strftime("%Y-%m-%d %H:%M:%S")
+
+def display(messages):
+    data = messages[:20]
+    word_plot = []
+    values_plot = []
+    for word in data:
+        word_plot.append(word[0])
+        values_plot.append(word[1])
+
+    fig = plt.figure(figsize = (10, 5))
+
+    # creating the bar plot
+    plt.bar(word_plot, values_plot, color ='maroon', 
+            width = 0.4)
+
+    plt.xlabel("Word")
+    plt.ylabel("Frequency")
+    plt.title(":D")
+    plt.show()
+
+def word_frequency(messages):
+    tokenized_count = {}
+
+    snow_stemmer = SnowballStemmer(language='english')
+
+    # Load in stop words
+    stop_words = [""]
+    with open(stop_list) as f:
+        for line in f:
+            stop_words.append(line.strip())
+
+    # Itterate thru every message
+    for message in messages:
+        # Itterate thru every word in each message
+        for word in message['body'].split():
+            word = word.lower() # Make lowercase
+            word = re.sub(r'[^\w\']+', '', word) # Remove all non a-Z charachters, non numbers but keep '
+
+            # Skip if in stop words
+            if word in stop_words:
+                continue
+
+            # word = snow_stemmer.stem(word)
+
+            # Add to list
+            if word.lower() not in tokenized_count:
+                tokenized_count[word] = 1
+            else:
+                tokenized_count[word] = tokenized_count[word]+1
+        
+    # Return an array of tuples sorted by the word count in reverse order
+    return sorted(tokenized_count.items(), key=lambda x: x[1], reverse=True)
+
+def emoji_frequency(messages):
+    exclude_list = ["￼", "�", "♂", "♀", "🏻", "🏼", "🏽", "🏾", "🏾"]
+
+    emoji_freq = {}
+    emoji_pattern = re.compile("["
+        u"\U0001F600-\U0001F64F"  # Emoticons
+        u"\U0001F300-\U0001F5FF"  # Miscellaneous Symbols and Pictographs
+        u"\U0001F680-\U0001F6FF"  # Transport and Map Symbols
+        u"\U0001F700-\U0001F77F"  # Alchemical Symbols
+        u"\U0001F780-\U0001F7FF"  # Geometric Shapes Extended
+        u"\U0001F800-\U0001F8FF"  # Supplemental Arrows-C
+        u"\U0001F900-\U0001F9FF"  # Supplemental Symbols and Pictographs
+        u"\U0001FA00-\U0001FA6F"  # Chess Symbols
+        u"\U0001FA70-\U0001FAFF"  # Symbols and Pictographs Extended-A
+        u"\U00002702-\U000027B0"  # Dingbat Symbols
+        u"\U000024C2-\U0001F251" 
+        "]+", flags=re.UNICODE)
+
+
+    for message in messages:
+        matches = emoji_pattern.findall(message['body'])
+        from_me = message['is_from_me']
+
+        # If there's no matches then skip the message
+        if len(matches) == 0:
+            continue
+
+        flatenned_matches = ""
+        for i in matches:
+            flatenned_matches += i
+
+        # Itterate thru every emoji in each message
+        for emoji in flatenned_matches:
+            if emoji in exclude_list:
+                continue
+
+            # Add to list
+            if emoji not in emoji_freq:
+                emoji_freq[emoji] = 1
+            else:
+                emoji_freq[emoji] = emoji_freq[emoji]+1
+    
+    # Return an array of tuples sorted by the word count in reverse order
+    return sorted(emoji_freq.items(), key=lambda x: x[1], reverse=True)
+
+def save_to_file(messages, filename, count=None):
+    file = open(filename, "+w")
+
+    if count == None:
+        count = len(messages)
+
+    for i in range(count):
+        file.write( f"{messages[i][0]}, {messages[i][1]}\n" )
+
+if __name__ == "__main__":
+    messages = read_messages(chat_db, n=n, self_number=self_number, human_readable_date=True, handle_identifyer=89)
+    
+    # for message in messages:
+        # print(message['body'])
+        # print(type(message['date']))
+        # print(print_date(message['date']))
+
+
+    # word_count = word_frequency(messages)
+    emoji_count = emoji_frequency(messages)
+    save_to_file(emoji_count, "adri_emoji.csv")
